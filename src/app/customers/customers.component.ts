57fd931547fb615d2520d83bacd1b35dea6b5fc0
Saravanava3 <Saravanava3@gmail.com>
2018-09-04 19:15:24 +0530

    Implemented lazy loading using angular 6

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,12 @@
+ import { Component, OnInit } from '@angular/core';
+
+ @Component({
+     selector: 'app-customer',
+     template: ''
+ })
+
+ export class CustomerComponent implements OnInit {
+     ngOnInit( ) {
+
+     }
+ }
